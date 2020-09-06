a611373f71eb0a0cc5291b3ff5e0bd7dca7db2c4
Henock Adane <adanehenock@gmail.com>
2020-09-06 10:37:10 +0100

    adding build file

---
+++ build/precache-manifest.e38f28bcc9b70f6d943ef263f3080dde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16eaefbc5cb30e10e8b5eb5639c8603e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81c9967cb340c5655a34",
+    "url": "/static/css/main.ae827603.chunk.css"
+  },
+  {
+    "revision": "b86812edfee35c3ba558",
+    "url": "/static/js/2.ee7ee6e4.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.ee7ee6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c9967cb340c5655a34",
+    "url": "/static/js/main.f484d04f.chunk.js"
+  },
+  {
+    "revision": "51433c4431c825c493fc",
+    "url": "/static/js/runtime-main.a10e7cf3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
